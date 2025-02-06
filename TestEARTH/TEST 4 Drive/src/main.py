8cf6bb1e423e72262a148d7e0af4af2bbf4c61d6
Akarawin <akarawinkulkaew@gmail.com>
2025-02-06 15:23:00 +0700

    FWD 2 Class

--- TestEARTH/TEST 4 Drive/src/main.py
+++ TestEARTH/TEST 4 Drive/src/main.py
@@ -19,13 +19,19 @@ motor_8 = Motor(Ports.PORT8, 1.0, True)
 motor_9 = Motor(Ports.PORT9, 1.0, True)
 motor_10 = Motor(Ports.PORT10, 1.0, True)
 
+MClass1 = motor_7
+MClass2 = motor_8
+MClass3 = motor_9
+MClass4 = motor_10
+
+
 class Motor_Control:
     class Forward:
         def __init__(self,Velocity):
-            self.M1 = motor_7
-            self.M2 = motor_8
-            self.M3 = motor_9
-            self.M4 = motor_10
+            self.M1 = MClass1
+            self.M2 = MClass2
+            self.M3 = MClass3
+            self.M4 = MClass4
             self.Velocity = Velocity
         def TWD(self):
             self.M1.spin(FORWARD,self.Velocity)
@@ -38,10 +44,10 @@ class Motor_Control:
 
     class Stop:
         def __init__(self,StopTypes):
-            self.M1 = motor_7
-            self.M2 = motor_8
-            self.M3 = motor_9
-            self.M4 = motor_10
+            self.M1 = MClass1
+            self.M2 = MClass2
+            self.M3 = MClass3
+            self.M4 = MClass4
             self.StopTypes = StopTypes
         def TWD(self):
             self.M1.stop()
@@ -63,7 +69,7 @@ class Motor_Control:
 
 MotorForward = Motor_Control.Forward(100)
 MotorStop = Motor_Control.Stop(BRAKE)
-MotorForward.TWD()
+MotorForward.FWD()
 wait(1,SECONDS)
-MotorStop.TWD()
+MotorStop.FWD()
 
