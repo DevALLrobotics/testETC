5073b44cf07f71e778dd5717ba791720f0ce766b
Jarvis_N <non.ny1999@hotmail.com>
2025-02-06 12:04:46 +0700

    setup testVEX IQ

---
+++ testVIQ/src/main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       jarvisn                                                      #
+# 	Created:      2/6/2025, 12:02:59 PM                                        #
+# 	Description:  IQ2 project                                                  #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello IQ2")
