8d50df7cb53ac15d55427d341fdd72f2055b257a
Jarvis_N <non.ny1999@hotmail.com>
2025-03-09 02:14:31 +0700

    Update testAPI.py

--- TestWebIQ/api/testAPI.py
+++ TestWebIQ/api/testAPI.py
@@ -1,4 +1,4 @@
-import mariadb
+import mariadb # type: ignore
 import os
 from flask import Flask, request, jsonify
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
